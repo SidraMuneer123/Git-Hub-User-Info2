4931cb200c08a2a918f5c818e2c078acc5e053be
New-repo <sidra182001@gmail.com>
2023-10-03 19:54:05 +0500

    Adding project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Detail from './Detail';
+import {Route , Routes} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import Home from './Home';
 
-function App() {
+
+const App = () => {
+  const [searchterm , setSearchTerm] = useState('');
+  const[data,setData]=useState('')
+  const handleclick = async()=>{
+    try{
+      const res = await fetch(`https://api.github.com/users/${searchterm}`);
+      const response =  await res.json();
+      console.log(response);
+      setData(response)
+    }
+    catch(error){
+      console.error('U got an  error' , error);
+
+    }
+
+  }
+  const change = (event)=>{
+    setSearchTerm(event.target.value)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+<Routes>
+<Route exact path='/' element = {<Home searchterm = {searchterm} change = {change} handleclick = {handleclick} data = {data}/>}></Route>
+      {/* <i class="fa-solid fa-magnifying-glass" style={{'color': '#6193ea'}}></i> */}
+<Route exact path='/detail' element={<Detail userdata = {data} />} /> 
+    </Routes>
+{/* <NavLink to='/'>Home</NavLink> */}
+      {/* // <Detail userdata = {data} /> */}
+
+    </>
+  )
 }
 
-export default App;
+export default App
